db20fe9fcc2d25107794988ae0edbff95e3d68b0
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-25 16:46:11 +0200

    Encapsulation des champs

--- src/main/java/ex1/entreprise.java
+++ src/main/java/ex1/entreprise.java
@@ -9,10 +9,46 @@ public class entreprise {
 	private String adresse;
 	private Date dateCreation;
 	
-	private static final int capitalMax = 3000000;
+	private static final int CAPITAL_MAX = 3000000;
 	
 	public void Afficher_statut(){
 		
+	}
+
+	public int getSiret() {
+		return siret;
+	}
+
+	public void setSiret(int siret) {
+		this.siret = siret;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+	public static int getCapitalMax() {
+		return CAPITAL_MAX;
 	}
 	
 }
