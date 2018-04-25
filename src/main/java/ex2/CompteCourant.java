e33494efd9172c4796308caf08108f722ce9e86d
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-25 17:05:37 +0200

    Ajout constructeur pour initialiser un decouvert

--- src/main/java/ex2/CompteCourant.java
+++ src/main/java/ex2/CompteCourant.java
@@ -9,6 +9,11 @@ public class CompteCourant extends CompteBancaire {
 		this.decouvert = 0;
 	}
 	
+	public CompteCourant(String type, double solde, double tauxRemuneration, double decouvert) {
+		super("CC", solde, 0, tauxRemuneration);
+		this.decouvert = decouvert;
+	}
+	
 	public void debiterMontant(double montant)
 	{
 		if (this.solde - montant > decouvert){
